d27194c872eff03bf82173633614a4d480fd1416
Marvin Viverge <marvin.viverge@gmail.com>
2022-12-29 23:11:29 +0100

    finishing index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,25 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, id, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const article = document.createElement('article');
+        article.insertAdjacentHTML(
+            "beforeend",
+            `
+                <a href="./photographer.html?id=${data.id}">
+                    <img src="${picture}" alt="">
+                    <h2>${data.name}</h2>
+                </a>        
+                <h3>${data.city}, ${data.country}</h3>
+                <p class="tagline">${data.tagline}</p>
+                <p class="price">${data.price}€/jour</p>
+            `
+        );
+
         return (article);
     }
+
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Création d'une fonction principale "main" qui va appeler les autres fonctions
+    async function main() {
+
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
+
+    main();
+
+    // Création d'une fonction pour récupérer les données de l'API
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return fetch('http://localhost:5500/data/photographers.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
+    // Création d'une fonction pour intégrer/afficher au HTML les données provenant de l'API
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -36,11 +31,4 @@
         });
     };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+})
